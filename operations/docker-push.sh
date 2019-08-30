aaecfb2425bce8c5ffaa0ed344ae861ed45439ec
pascallosss <53646294+pascallosss@users.noreply.github.com>
2019-08-30 21:50:42 +0200

    Update docker-push.sh

--- operations/docker-push.sh
+++ operations/docker-push.sh
@@ -8,11 +8,7 @@ cd ${base_dir}
 echo ${base_dir}
 
 set -e
-git config --global user.email "${ghprbActualCommitAuthorEmail:-github@bjoola.nl}"
-git config --global user.name "${GITHUB_ACTOR:-GitHub}"
-[ -n "${GITHUB_SHA}" ] && export GIT_COMMIT=${ghprbActualCommit}
-
-export VERSION=${APP_VERSION}-${GIT_COMMIT:0:10}
+export VERSION=${APP_VERSION}-${GITHUB_SHA:0:10}
 
 echo ${VERSION}
 echo ${GIT_COMMIT}
