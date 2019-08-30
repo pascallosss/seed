abf6e007df2b976ec25453a242476c9d53bd7c43
pascallosss <53646294+pascallosss@users.noreply.github.com>
2019-08-30 21:35:51 +0200

    Update ci-script.sh

--- operations/ci-script.sh
+++ operations/ci-script.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
+cd /github/workspace/operations
+
 export $(cat ../.version | xargs)
 
 # /usr/local/bin/dobi ci-lint
 
-cd /github/workspace/operations
-
 chmod +x docker-push.sh
 
-sh ./docker-push.sh
+sh docker-push.sh
