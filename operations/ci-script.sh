29299619f66e8c2c75061d039ad119bcfcf70b5c
pascallosss <53646294+pascallosss@users.noreply.github.com>
2019-08-30 21:00:36 +0200

    Update ci-script.sh

--- operations/ci-script.sh
+++ operations/ci-script.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/usr/bin/env bash
 
 export $(cat ./.version | xargs)
 
@@ -6,4 +6,4 @@ export $(cat ./.version | xargs)
 
 chmod +X /github/workspace/operations/docker-push.sh
 
-source /github/workspace/operations/docker-push.sh
+./github/workspace/operations/docker-push.sh
